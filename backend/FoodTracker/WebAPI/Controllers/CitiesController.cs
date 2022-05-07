41a9f66bf08562e84b77eb8c2584267c650c7d8c
sahinboga <bogasahin967@gmail.com>
2022-05-07 17:38:25 +0300

    add webapi

---
+++ backend/FoodTracker/WebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstracts;
+using Core.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CitiesController : ControllerBase
+	{
+		ICityService _cityService;
+
+		public CitiesController(ICityService cityService)
+		{
+			_cityService = cityService;
+		}
+
+		[HttpGet("getallcities")]
+		public IActionResult GetAll()
+		{
+			return this.ResponseResult(_cityService.GetAll());
+		}
+	}
+}
