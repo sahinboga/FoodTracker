98e2f602b965fe72819aafd3f01849e04cae5834
sahinboga <bogasahin967@gmail.com>
2022-05-07 11:02:21 +0300

    .

--- backend/FoodTracker/Core/obj/Debug/net5.0/Core.AssemblyInfo.cs
+++ backend/FoodTracker/Core/obj/Debug/net5.0/Core.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("762d59b9-e70d-4627-8fa7-9d190841531e")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
