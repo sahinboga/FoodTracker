20bdb5e144d59f3448212dab3e9c50787ebae239
sahinboga <bogasahin967@gmail.com>
2022-05-07 12:01:06 +0300

    Add DataAccess process

---
+++ backend/FoodTracker/DataAccess/Concretes/EntityFramework/FoodTrackerContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concretes.EntityFramework
+{
+	public class FoodTrackerContext:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=FoodTracker;Trusted_Connection=true");
+		}
+
+		public DbSet<Restaurant> Restaurants { get; set; }
+		public DbSet<RestaurantImage> RestaurantImages { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<City> Cities { get; set; }
+		public DbSet<County> Counties { get; set; }
+	}
+}
