2061a2def019e3fd276edd29aed6f37fadf68e9d
sahinboga <bogasahin967@gmail.com>
2022-05-07 10:39:30 +0300

    Add Entity process

---
+++ backend/FoodTracker/Entities/Concretes/County.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+	public class County:IEntity
+	{
+		public int Id { get; set; }
+		public int CityId { get; set; }
+		public string CountyName { get; set; }
+	}
+}
---
+++ backend/FoodTracker/Entities/Concretes/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+	public class Restaurant:IEntity
+	{
+		public int Id { get; set; }
+		public int CategoryId { get; set; }
+		public string ReastaurantName { get; set; }
+		public int CountyId { get; set; }
+		public DateTime FoundedDate { get; set; }
+	}
+}
