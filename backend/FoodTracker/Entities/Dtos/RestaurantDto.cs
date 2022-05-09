3d1b7d731f0a7c5ab02aa28707e28d3f2ed010e6
sahinboga <bogasahin967@gmail.com>
2022-05-09 17:13:54 +0300

    add Dto

---
+++ backend/FoodTracker/Entities/Dtos/RestaurantDto.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+	public class RestaurantDto:IEntity
+	{
+		public int Id { get; set; }
+		public string RestaurantName { get; set; }
+		public string Category { get; set; }
+		public string Location { get; set; }
+		public DateTime FoundedDate { get; set; }
+	}
+}
