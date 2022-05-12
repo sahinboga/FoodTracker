8c6e4ab8ffd09271d41412141b8d73510cc3e3d2
sahinboga <bogasahin967@gmail.com>
2022-05-12 15:39:27 +0300

    filtering city

---
+++ frontend/FoodTracker/src/app/pipes/countines-filter-by-cityid.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CountinesFilterByCityidPipe } from './countines-filter-by-cityid.pipe';
+
+describe('CountinesFilterByCityidPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CountinesFilterByCityidPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ frontend/FoodTracker/src/app/pipes/countines-filter-by-cityid.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { County } from '../models/county';
+
+@Pipe({
+  name: 'countinesFilterByCityid'
+})
+export class CountinesFilterByCityidPipe implements PipeTransform {
+
+  transform(value: County[], cityId: number): County[] {
+    console.log(cityId)
+    return cityId? value.filter(c=>c.cityId==cityId):value;
+  }
+
+}
