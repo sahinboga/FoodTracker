3d6bac0e1c82aa532f278399abc29ce6dacb118b
sahinboga <bogasahin967@gmail.com>
2022-05-18 11:07:05 +0300

    routing process

--- frontend/FoodTracker/src/app/app-routing.module.ts
+++ frontend/FoodTracker/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RestaurantDetailComponent } from './components/restaurant-detail/restaurant-detail.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"restaurants",component:RestaurantComponent},
+  {path:"restaurants/detail/:restaurantId",component:RestaurantDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
